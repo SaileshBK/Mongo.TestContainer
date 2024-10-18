a921b3c5e29c6dac99e71cef77b5e3daff93df45
SaileshBK <sailesh.bk1@icloud.com>
2024-10-17 21:08:28 -0500

    feat: Implemented Logs error endpoint

--- Mongo.TestContainer/Controllers/LogsController.cs
+++ Mongo.TestContainer/Controllers/LogsController.cs
@@ -9,10 +9,18 @@ public sealed class LogsController([FromKeyedServices(TestContainerKeys.MongoDbC
     private readonly MongoDbContainer _mongoContainer = mongoContainer;
 
     [HttpGet]
-    [Route("api/logs")]
-    public async Task<IActionResult> GetLogs()
+    [Route("api/container/logs")]
+    public async Task<IActionResult> GetContainerLogs()
     {
         var (stdout, _) = await _mongoContainer.GetLogsAsync();
         return Ok(stdout);
+    }
+
+    [HttpGet]
+    [Route("api/container/error")]
+    public async Task<IActionResult> GetContainerErrors()
+    {
+        var (_, error) = await _mongoContainer.GetLogsAsync();
+        return Ok(error);
     }
 }
